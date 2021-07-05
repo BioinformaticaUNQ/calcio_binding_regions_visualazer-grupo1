dd9b1a186f332cd59e8d1d0bd800cbed7e21ee8d
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-07-05 01:02:19 -0300

    Add Check PDB in app

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,11 +10,41 @@ window.lynxCBRV = {
         url: "https://neutralino.js.org/docs"
       });
     },
-    checkPDB: () => {
+    checkPDB: async () => {
+      div = document.getElementById("pdbNotify");
       if(pdbFile == null){
-        // Por favor seleccione un PDB
+        div.innerHTML=`
+        <div class="info-box warning col-12" style="position:relative;">
+          <div class="info-box-content">
+            Remember, it is necessary to select a PDB file in order to verify it.
+          </div>
+        </div>
+        `
       }else{
-        // Existe File
+        let com = 'python resources/scripts/verify.py '.concat(pdbFile);
+        console.log(com);
+        let response = await Neutralino.os.execCommand({
+          command: com
+        });
+        let resp = JSON.parse(response.output);
+        console.log(resp)
+        if (resp.code == 200) {
+          div.innerHTML=`
+          <div class="info-box success col-12" style="position:relative;">
+            <div class="info-box-content">
+              Your PDB file appears to be fine, we can get on with it.
+            </div>
+          </div>
+          `
+        }else{
+          div.innerHTML=`
+          <div class="info-box alert col-12" style="position:relative;">
+            <div class="info-box-content">
+              `.concat(resp.response).concat(`
+            </div>
+          </div>
+          `)
+        }
       }
     },
     openPDB: async () => {
