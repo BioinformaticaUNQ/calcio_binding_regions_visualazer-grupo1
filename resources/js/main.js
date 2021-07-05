ea913c433e69ebcfa343a0af2a103570e7f87f48
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-07-04 22:40:38 -0300

    Add Import File to PDB

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,15 +1,43 @@
+var pdbFile = null;
 
-window.myApp = {
+window.lynxCBRV = {
     showInfo: () => {
-        document.getElementById('info').innerHTML = NL_APPID + " is running on port " +
-                    NL_PORT + " inside " + NL_OS + ".<br/><br/>" + "<span>v" + NL_VERSION + "</span>";
+      document.getElementById('info').innerHTML = NL_APPID + " is running on port " +
+                  NL_PORT + " inside " + NL_OS + ".<br/><br/>" + "<span>v" + NL_VERSION + "</span>";
     },
     openDocs: () => {
-        Neutralino.app.open({
-            url: "https://neutralino.js.org/docs"
-        });
+      Neutralino.app.open({
+        url: "https://neutralino.js.org/docs"
+      });
+    },
+    checkPDB: () => {
+      if(pdbFile == null){
+        // Por favor seleccione un PDB
+      }else{
+        // Existe File
+      }
+    },
+    openPDB: async () => {
+      let response = await Neutralino.os.showDialogOpen({
+        title: 'Select Your PDB'
+      });
+      pdbFile = response.selectedEntry;
+      setPathInfo();
     }
 };
 
+// Funciones
+
+function setPathInfo(){
+  div = document.getElementById("divFilePath");
+  if (pdbFile) {
+    div.value = pdbFile;
+  }else{
+    div.value = `You didn't select any file`;
+  }
+}
+
+// Secuencia de inicio
+
 Neutralino.init();
-window.myApp.showInfo();
+setPathInfo();
