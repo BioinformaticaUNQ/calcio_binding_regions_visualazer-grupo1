cc47efe8cde2952dfdfafeddd257a66b87462da0
Pablo Saba <sabaliauskaspablo@gmail.com>
2021-07-14 01:07:31 -0400

    fix foldx

--- resources/scripts/openPymol.py
+++ resources/scripts/openPymol.py
@@ -1,5 +1,12 @@
+import json
 import sys
+
 import pymol
+from Bio.PDB import PDBParser
+
+from foldx import get_fold_data
+
+strict_parser = PDBParser(PERMISSIVE=0)
 
 # Call the function below before using any PyMOL modules.
 
@@ -10,19 +17,22 @@ pymol.finish_launching()
 
 from pymol import cmd
 
+path = sys.argv[1]
+data = get_fold_data(path)
+
 try:
-    path = sys.argv[1]
+
     try:
         myPDB = strict_parser.get_structure('name', path)
         cmd.load(path)
-        cmd.show("sticks")
-    except:
-        print(json.dumps({
-            "code":204,
-            "response":"ERROR: The PDB format is not correct"
-        }))
+        for i in data:
+            cmd.color("red", "byres all within 5 of resi " + str(i["number"]))
+
+    except Exception as e:
+        print(e)
+
 except:
     print(json.dumps({
-        "code":204,
-        "response":"ERROR: You must enter the path of the PDB"
+        "code": 204,
+        "response": "ERROR: You must enter the path of the PDB"
     }))
