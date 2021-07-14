e5eba757fb395260bb43981cac037eb9e1464cf2
Pablo Saba <sabaliauskaspablo@gmail.com>
2021-07-14 01:31:07 -0400

    verify changend

--- resources/scripts/verify.py
+++ resources/scripts/verify.py
@@ -3,24 +3,35 @@ import json
 
 from Bio.PDB import *
 
-strict_parser = PDBParser(PERMISSIVE=0)
-myPDB = None
 
-try:
-    path = sys.argv[1]
+def validate_pdb():
     try:
-        myPDB = strict_parser.get_structure('name', path)
-        print(json.dumps({
-            "code":200,
-            "response": "OK: PDB file is correct"
-        }))
+        path = sys.argv[1]
+        try:
+            parser = PDBParser(PERMISSIVE=0)
+            structure = parser.get_structure("name", path)
+            for model in structure:
+                for chain in model:
+                    for residue in chain:
+                        if residue.resname == "CA":
+                            print(json.dumps({
+                                "code": 200,
+                                "response": "OK: PDB file is correct"
+                            }))
+                            return
+
+            print(json.dumps({
+                "code": 204,
+                "response": "ERROR: There are no calcium ion bonds in the protein "
+            }))
+        except:
+            print(json.dumps({
+                "code": 204,
+                "response": "ERROR: The PDB format is not correct"
+            }))
     except:
         print(json.dumps({
-            "code":204,
-            "response":"ERROR: The PDB format is not correct"
+            "code": 204,
+            "response": "ERROR: You must enter the path of the PDB"
         }))
-except:
-    print(json.dumps({
-        "code":204,
-        "response":"ERROR: You must enter the path of the PDB"
-    }))
+validate_pdb()
